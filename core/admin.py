4357127b1338f135b36a8da0e1979c355c57e69b
jlbarrera <jl.barrera.trancoso@gmail.com>
2014-02-14 18:14:25 +0100

    updating models

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from core.models import *
 
 admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(External)
 admin.site.register(Entity)
+admin.site.register(Company)
+admin.site.register(CashFlow)
+admin.site.register(Sale)
+admin.site.register(Discount)
+admin.site.register(Number)
+admin.site.register(Outlay)
+admin.site.register(Category)
--- core/models.py
+++ core/models.py
@@ -3,48 +3,48 @@ from django.contrib.auth.models import User
 from multitenant.models import TenantModel
 
 class Person(TenantModel):
-    passport = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)    
-    surname = models.CharField(max_length=200)
-    mobile = models.IntegerField()
-    telephone = models.IntegerField()
-    email = models.EmailField()
-    address = models.CharField(max_length=200)
-    postal_code = models.IntegerField()
-    city = models.CharField(max_length=200)
-    province = models.CharField(max_length=200)
-    gender = models.CharField(max_length=200)
-    born = models.DateField()
+    name = models.CharField(max_length=200)
+    passport = models.CharField(max_length=200, blank=True)        
+    surname = models.CharField(max_length=200, blank=True)
+    mobile = models.IntegerField(blank=True)
+    telephone = models.IntegerField(blank=True)
+    email = models.EmailField(blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    postal_code = models.IntegerField(blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    province = models.CharField(max_length=200, blank=True)
+    gender = models.CharField(max_length=200, blank=True)
+    born = models.DateField(blank=True)
     
     class Meta:
         abstract = True
 
 class Company(TenantModel):
     name = models.CharField(max_length=200)
-    code = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    province = models.CharField(max_length=200)
-    postal_code = models.IntegerField()
-    telephone = models.IntegerField()
-    email = models.EmailField()
+    code = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    province = models.CharField(max_length=200, blank=True)
+    postal_code = models.IntegerField(blank=True)
+    telephone = models.IntegerField(blank=True)
+    email = models.EmailField(blank=True)
 
-class Employee(Person):
-    user = models.OneToOneField(User)
+class Employee(Person):    
     type = models.CharField(max_length=200)
-    paysheet = models.FloatField()
-    company_cost = models.FloatField()
+    paysheet = models.FloatField(blank=True)
+    company_cost = models.FloatField(blank=True)    
+    user = models.OneToOneField(User)
     company = models.ForeignKey(Company)
 
 class Customer(Person):
-    notes = models.CharField(max_length=8000)
+    notes = models.CharField(max_length=8000, blank=True)
 
 class External(Person):
     assigment = models.FloatField()
 
 class Entity(TenantModel):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     price = models.IntegerField()
     created = models.DateTimeField()
     
@@ -53,26 +53,26 @@ class CashFlow(TenantModel):
     datetime = models.DateTimeField()
     type = models.CharField(max_length=200)
     paymentform = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     customized_amount = models.BooleanField()
     paid = models.BooleanField()
 
 class Sale(CashFlow):        
     customer = models.ForeignKey(Customer)
-    external = models.ForeignKey(External)
+    external = models.ForeignKey(External, blank=True)
 
 class Discount(TenantModel):
     percentage = models.IntegerField()
     sale = models.ForeignKey(Sale)
-    entity = models.ForeignKey(Entity)
+    entity = models.ForeignKey(Entity)    
 
 class Number(TenantModel):
     number = models.IntegerField()
-    sale = models.ForeignKey(Sale)
+    sale = models.ForeignKey(Sale)    
     entity = models.ForeignKey(Entity)
       
 class Outlay(CashFlow):
-    dateofpaid = models.DateField()
+    dateofpaid = models.DateField(blank=True)
     
-class Categoty(TenantModel):
+class Category(TenantModel):
     name = models.CharField(max_length=200)
