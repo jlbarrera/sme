c2f67cbd6a5ab8f9beebb1514be6dfafa92b1b7b
jlbarrera <jl.barrera.trancoso@gmail.com>
2014-05-08 14:25:20 +0200

    DateTimePicker and events form

--- sme/settings.py
+++ sme/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'multitenant',
     'core',
     'smecal',
--- smecal/models.py
+++ smecal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from core.models import Entity, Customer, Employee
 from django.utils.translation import ugettext_lazy as _
 from multitenant.forms import TenantModelForm
@@ -22,4 +23,6 @@ class EventForm(TenantModelForm):
         exclude = ['tenant', 'name']
         
     def __init__(self, *args, **kwargs):
-        super(EventForm, self).__init__(*args, **kwargs)  
+        super(EventForm, self).__init__(*args, **kwargs)
+    
+    when = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'form-control', 'placeholder':_('Enter DateTime')}))  
